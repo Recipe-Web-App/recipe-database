f7591292b776663b47b79c3e977f9a8466b42f39
jsamuelsen <jsamuelsen11@gmail.com>
2025-08-18 17:59:44 -0400

    refactor: Updated review to support 0.0 & 0.5 values.

--- db/init/schema/011_create_recipe_reviews_table.sql
+++ db/init/schema/011_create_recipe_reviews_table.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS recipe_manager.reviews (
     user_id
   ) ON DELETE CASCADE,
   rating NUMERIC(2, 1) NOT NULL CHECK (
-    rating IN (1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0)
+    rating IN (0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0)
   ),
   comment TEXT,
   created_at TIMESTAMPTZ DEFAULT now()
